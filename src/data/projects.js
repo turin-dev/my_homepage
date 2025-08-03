6bee86524db3d3589ca4f7544f50360a9b636384
투린 <Koomj5258@gmail.com>
2025-08-03 18:42:37 +0900

    feat: initialize React + Tailwind homepage

---
+++ src/data/projects.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    title: 'Birthday Bot',
+    description: '친구들의 생일을 알려주는 디스코드 봇',
+    link: 'https://github.com/example/birthday-bot',
+  },
+];
+
+export default projects;
